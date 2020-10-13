8ac840918647b9531da16c6308b3cb6f27a7c0e2
lixin <lxhuahua001@163.com>
2020-10-13 09:56:43 +0800

    24. 两两交换链表中的节点

---
+++ src/SwapPairs/SwapPairs.java
@@ -0,0 +1,35 @@
+package SwapPairs;
+
+/**
+ * @description:
+ * @author: wwh
+ * @create: 2020/10/13
+ */
+public class SwapPairs {
+    public static void main(String[] args) {
+
+    }
+
+    public ListNode swapPairs(ListNode head) {
+
+                if (head == null || head.next == null) {
+                    return head;
+                }
+                ListNode newHead = head.next;
+                head.next = swapPairs(newHead.next);
+                newHead.next = head;
+                return newHead;
+
+        }
+    }
+ class ListNode {
+    int val;
+
+    ListNode next;
+
+    ListNode() {}
+
+    ListNode(int val) { this.val = val; }
+
+    ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+}
